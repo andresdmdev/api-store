5164bd3557d2a720c9bb9605e2a170c2a7a38191
Andres Marquez <andresdmf55@gmail.com>
2022-06-24 18:34:07 -0500

    Adding models and requests for /api/products

--- src/api/models/productsModel.js
+++ src/api/models/productsModel.js
@@ -1,20 +1,48 @@
 const pool = require('../../config/database')
 
-const services = {}
+const models = {}
 
-services.allProducts = async () => {
+models.allProducts = async () => {
   const promisePool = pool.promise()
 
   const [products] = await promisePool.query('SELECT * FROM productsAdidas')
   return { products }
 }
 
-services.addProduct = async (product) => {
-  const { products } = await services.allProducts()
+models.findProductById = async (id) => {
+  const promisePool = pool.promise()
+
+  const [product] = await promisePool.query(`SELECT * FROM productsAdidas WHERE id='${id}'`)
+  return product
+}
+
+models.searchProductByName = async (name) => {
+  const promisePool = pool.promise()
+
+  const [products] = await promisePool.query(`SELECT * FROM productsAdidas WHERE name LIKE '%${name}%'`)
+  return products
+}
+
+models.productsByLocation = async (idLocation) => {
+  const promisePool = pool.promise()
+
+  const [products] = await promisePool.query(`SELECT * FROM productsAdidas WHERE idLocation='${idLocation}'`)
+  return products
+}
+
+models.productsByCategory = async (idCategory) => {
+  const promisePool = pool.promise()
+
+  const [products] = await promisePool.query(`SELECT * FROM productsAdidas WHERE idCategory='${idCategory}'`)
+  return products
+}
+
+models.addProduct = async (product) => {
+  const { products } = await models.allProducts()
   const findProduct = products.find(elem => elem.id === product.id)
 
   if (findProduct !== undefined) {
-    return { 'No-valid-id': 'Ya se ha ingresado un producto con ese id, por favor introduce otro' }
+    throw Error('Ya se ha ingresado un producto con ese id, por favor introduce otro')
   } else {
     const promisePool = pool.promise()
 
@@ -30,4 +58,40 @@ services.addProduct = async (product) => {
   }
 }
 
-module.exports = services
+models.updateProduct = async (product) => {
+  const { products } = await models.allProducts()
+  const findProduct = products.find(elem => elem.id === product.id)
+
+  if (!findProduct) {
+    throw Error('No-valid-id - No se ha registrado un producto con ese id, por favor introduce otro')
+  } else {
+    const promisePool = pool.promise()
+
+    await promisePool
+      .query(`UPDATE productsAdidas SET name='${product.name}', sellingPrice='${product.sellingPrice}', 
+        discount='${product.discount}', color='${product.color}', idCategory='${product.idCategory}', 
+        idLocation='${product.idLocation}', breadcrumbs='${product.breadcrumbs}', description='${product.description}',
+        brand='${product.brand}', image1='${product.image1}', image2='${product.image2}',
+        image3='${product.image3}', averageRating='${product.averageRating}' WHERE id='${product.id}'
+      `)
+
+    return product
+  }
+}
+
+models.deleteProduct = async (idProduct) => {
+  const { products } = await models.allProducts()
+  const findProduct = products.find(elem => elem.id === idProduct)
+
+  if (!findProduct) {
+    throw Error('No existe un producto asociado con el id, verificar id')
+  } else {
+    const promisePool = pool.promise()
+
+    await promisePool.query(`DELETE FROM productsAdidas WHERE id='${idProduct}'`)
+
+    return { message: 'Producto eliminado exitosamente' }
+  }
+}
+
+module.exports = models
