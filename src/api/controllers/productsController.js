7aa70b00bc8678b14e5e6aa718653f64527d4ce1
Andres Marquez <andresdmf55@gmail.com>
2022-06-24 18:32:41 -0500

    Adding controllers for /api/products

--- src/api/controllers/productsController.js
+++ src/api/controllers/productsController.js
@@ -1,7 +1,22 @@
-const { allProducts, addProduct } = require('../models/productsModel')
-const { formatedProductsArray, formatedProductObject, validateProductData } = require('../services/productsService')
+const {
+  allProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  findProductById,
+  searchProductByName,
+  productsByLocation,
+  productsByCategory
+} = require('../models/productsModel')
 
-// Controllers of Products
+const {
+  formatedProductsArray,
+  formatedProductObject,
+  validateProductData,
+  validateIdProduct
+} = require('../services/productsService')
+
+// Products Controllers
 
 const controller = {}
 
@@ -14,11 +29,68 @@ controller.allProducts = async (req, res) => {
   }
 }
 
+controller.findProductById = async (req, res) => {
+  try {
+    const id = validateIdProduct(req.params.id)
+    const data = await findProductById(id)
+    res.status(200).json(formatedProductsArray(data)).end()
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
+}
+
+controller.searchProductByName = async (req, res) => {
+  try {
+    const name = await searchProductByName(req.params.name)
+    res.status(200).json(formatedProductsArray(name)).end()
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
+}
+
+controller.productsByLocation = async (req, res) => {
+  try {
+    const data = await productsByLocation(req.params.idLocation)
+    res.status(200).json(formatedProductsArray(data)).end()
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
+}
+
+controller.productsByCategory = async (req, res) => {
+  try {
+    const data = await productsByCategory(req.params.idCategory)
+    res.status(200).json(formatedProductsArray(data)).end()
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
+}
+
 controller.addProduct = async (req, res) => {
   try {
     const body = validateProductData(req.body)
     const data = await addProduct(body)
     res.status(200).json(formatedProductObject(data)).end()
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
+}
+
+controller.updateProduct = async (req, res) => {
+  try {
+    const body = validateProductData(req.body)
+    const data = await updateProduct(body)
+    res.status(200).json(formatedProductObject(data)).end()
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
+}
+
+controller.deleteProduct = async (req, res) => {
+  try {
+    const id = validateIdProduct(req.params.id)
+    const data = await deleteProduct(id)
+    res.status(200).json(data).end()
   } catch (error) {
     res.status(404).json(error.message)
   }
