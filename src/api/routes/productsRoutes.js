504acd396968420a20f98c73124287bbad294127
Andres Marquez <andresdmf55@gmail.com>
2022-06-24 18:31:55 -0500

    Adding new routes for /api/products

--- src/api/routes/productsRoutes.js
+++ src/api/routes/productsRoutes.js
@@ -1,9 +1,30 @@
 const { Router } = require('express')
 const route = Router()
-const { allProducts, addProduct } = require('../controllers/productsController')
+const {
+  allProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  findProductById,
+  searchProductByName,
+  productsByLocation,
+  productsByCategory
+} = require('../controllers/productsController')
 
 route.get('/', allProducts)
 
+route.get('/:id', findProductById)
+
+route.get('/name/:name', searchProductByName)
+
+route.get('/orderByLocation/:idLocation', productsByLocation)
+
+route.get('/orderByCategory/:idCategory', productsByCategory)
+
 route.post('/', addProduct)
+
+route.put('/', updateProduct)
+
+route.delete('/deleteProduct/:id', deleteProduct)
 
 module.exports = route
