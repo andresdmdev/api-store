5e64ab8635bb9c4936c8f3cf35df94fd0aabbc15
Andres Marquez <andresdmf55@gmail.com>
2022-06-28 18:40:00 -0500

    Config Heroku

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express()
 
 // Middlewares Cors y Morgan
 app.use(require('./api/middlewares/middlewares').morgan2)
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.set('port', process.env.PORT || 3000)
@@ -15,6 +16,11 @@ app.use('/api/users', require('./api/routes/usersRoutes'))
 
 // Shopping Routes
 app.use('/api/shopping', require('./api/routes/shoppingRoutes'))
+
+// Error 404
+app.use((req, res) => {
+  res.status(404).send('Error: Not found or page doesn\'t exist')
+})
 
 app.listen(app.get('port'), () => {
   console.log('Server on port ' + app.get('port'))
