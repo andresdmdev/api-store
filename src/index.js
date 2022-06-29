dc8752e2bcb43963127c0808fcb34dd57c717b07
Andres Marquez <andresdmf55@gmail.com>
2022-06-28 19:00:26 -0500

    Deploy

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.set('port', process.env.PORT || 3000)
+
 // Middlewares Cors y Morgan
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
-app.set('port', process.env.PORT || 3000)
 
 // Product Routes
 app.use('/api/products', require('./api/routes/productsRoutes'))
